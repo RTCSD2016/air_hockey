c5630313e173a940ab304e6c3c210340153c2bee
Ran Wang <rwang0417@gmail.com>
2017-01-05 21:19:49 +0800

    Update physics_engine.cpp

--- src/physics engine/physics_engine.cpp
+++ src/physics engine/physics_engine.cpp
@@ -9,6 +9,12 @@ namespace task_physics_engine {
     {
         radius = puck_radius;
         mass = puck_mass;
+        
+        cur_position[X] = 50;
+		cur_position[Y] = 100;
+
+		velocity[X] = 10;
+		velocity[Y] = 0;
     }
 
     Table::Table(double table_length, double table_width, double goal_length)
