7d9e36fbcf0bb23c050fde1418e51d6f6eff9e70
Ran Wang <rwang0417@gmail.com>
2017-01-05 12:42:58 +0800

    Update physics_engine.h

--- src/tasks/physics_engine.h
+++ src/tasks/physics_engine.h
@@ -12,46 +12,42 @@ namespace task_physics_engine {
     void main(void *arg);
 
 	enum direction_vector{ X, Y, AXIS_COUNT };
-	
+	enum flag1{ ungoal, win,lose }goal_flag;//进球标志
+	enum flag2{ table_col, stick_col }collision_flag;//碰撞标志
+
 	static double default_puck_radius = 0;
 	static double default_puck_mass = 0;
-	
+
 	static double default_table_length = 0;
 	static double default_table_width = 0;
-	
+
 	static double default_hockey_stick_radius = 0;
 	static double default_hockey_stick_mass = 0;
-
+	
+	static double default_goal_length1 = 0;
+	static double default_goal_length2 = 0;
+	
 class Puck{
 private:
-	void check_collision();
+	double collision_time;
+	double collision_position[AXIS_COUNT];
+
+	void check_collision();//检查碰撞
+	void update_position();//更新位置
+	void update_velocity();//更新速度
+
 protected:
 
 public:
 	double radius;
-	double position[AXIS_COUNT];
+	double mass;
+	double pre_position[AXIS_COUNT], cur_position[AXIS_COUNT];//上一步和现在的位置
 	double velocity[AXIS_COUNT];
 	double mass;
 
-	Puck(double puck_radius = default_puck_radius, double puck_mass = default_puck_mass){};
 	void dWorldStep();//更新物理状态
+	Puck(double r = default_puck_radius, double m = default_puck_mass);
 };
-
-Puck::Puck(double puck_radius, double puck_mass)
-{
-	radius = puck_radius;
-	mass = puck_mass;
-}
-
-void Puck::check_collision()
-{
-
-}
-
-void Puck::dWorldStep()
-{
-
-}
 
 class Table{
 private:
@@ -61,38 +57,25 @@ protected:
 public:
 	double length;
 	double width;
-	Table(double table_length = default_table_length, double table_width = default_table_width){};
-};
+	double goalline1;
+	double goalline2;
 
-Table::Table(double table_length, double table_width)
-{
-	length = table_length;
-	width = table_width;
-}
+	Table(double table_length = default_table_length, double table_width = default_table_width, double goal_length1 = default_goal_length1, double goal_length2 = default_goal_length2){};
+};
 
 class Hockey_stick{
 private:
 	void stick_checkstep(){};
 protected:
-
+	
 public:
 	double radius;
 	double mass;
-	double position[AXIS_COUNT];
+	double pre_position[AXIS_COUNT], cur_position[AXIS_COUNT];
 	double velocity[AXIS_COUNT];
-	Hockey_stick(double hockey_stick_radius = default_hockey_stick_radius, double hockey_stick_mass = default_hockey_stick_mass){};
+
+	Hockey_stick(double hockey_stick_radius = default_hockey_stick_radius, double hockey_stick_mass = default_hockey_stick_mass){};				
 };
-
-Hockey_stick::Hockey_stick(double hockey_stick_radius, double hockey_stick_mass)
-{
-	radius = default_hockey_stick_radius;
-	mass = default_hockey_stick_mass;
-}
-
-void Hockey_stick::stick_checkstep()
-{
-
-}
 }
 
 #endif //AIR_HOCKEY_PHYSICS_ENGINE_H
