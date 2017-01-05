240160c8a6ce89a17b6847820b0490bfe5deffbc
Ran Wang <rwang0417@gmail.com>
2017-01-05 20:43:30 +0800

    Update physics engine.cpp

--- src/physics engine/physics engine.cpp
+++ src/physics engine/physics engine.cpp
@@ -1,1 +1,19 @@
-
+Puck::Puck(double puck_radius, double puck_mass)
+    {
+        radius = puck_radius;
+        mass = puck_mass;
+    }
+    
+    Table::Table(double table_length, double table_width, double goal_length1, double goal_length2)
+    {
+        length = table_length;
+        width = table_width;
+        goalline1 = goal_length1;
+        goalline2 = goal_length2;
+    }
+    
+    Hockey_stick::Hockey_stick(double hockey_stick_radius, double hockey_stick_mass)
+    {
+        radius = default_hockey_stick_radius;
+        mass = default_hockey_stick_mass;
+    }
