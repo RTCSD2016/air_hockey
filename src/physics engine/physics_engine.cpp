40da2df893f854f3d5455078800b641f4c390ad1
Ran Wang <rwang0417@gmail.com>
2017-01-06 11:26:05 +0800

    Update physics_engine.cpp

--- src/physics engine/physics_engine.cpp
+++ src/physics engine/physics_engine.cpp
@@ -13,26 +13,41 @@ namespace task_physics_engine {
         cur_position[X] = 50;
         cur_position[Y] = 100;
 
-        velocity[X] = 10;
+        velocity[X] = 0;
         velocity[Y] = 0;
     }
     
     void Puck::check_collision()
     {
         if(cur_position[X] <= 5 || cur_position[X] >= 95) velocity[X] = -velocity[X];
-        if(cur_position[Y] <= 5 || cur_position[Y] >= 195) velocity[Y] = -velocity[Y];//撞桌子
+        if(cur_position[Y] <= 5 || cur_position[Y] >= 195)
+        {
+            if(cur_position[X] < 65 && cur_position[X] > 35)
+            {
+                if(cur_position[Y] <= 5) goal_flag = 1;
+                if(cur_position[Y] >= 195) goal_flag = 2;
+
+                velocity[X] = 0;
+                velocity[Y] = 0;
+            } else {velocity[Y] = -velocity[Y];}
+        }
         
         if((cur_position[X] - ball_x.position) * (cur_position[X] - ball_x.position) + (cur_position[Y] - ball_y.position) * (cur_position[Y] - ball_y.position) <= 225)
     }
     
     void Puck::update_position()
     {
-    
+        if(goal_flag > 0)
+        {
+            cur_position[X] = 50;
+            cur_position[Y] = 100;
+        }
     }
     
     void Puck::update_velocity()
     {
-    
+        ball_x.velocity = velocity[X];
+        ball_y.velocity = velocity[Y];
     }
 
     Table::Table(double table_length, double table_width, double goal_length)
