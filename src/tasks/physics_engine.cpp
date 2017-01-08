4aad7460cd744612fd2341655ad30164221c578e
Ran Wang <rwang0417@gmail.com>
2017-01-08 12:52:37 +0800

    Update physics_engine.cpp

--- src/tasks/physics_engine.cpp
+++ src/tasks/physics_engine.cpp
@@ -10,10 +10,18 @@
 #include "common.h"
 
 namespace task_physics_engine {
+     
     void main(void *arg) {
         rt_printf("[physics_engine] hello\n");
 
         unsigned long mask;
+        static double pre_time;
+        static double cur_time;
+        static double step_time;
+        
+        goal_flag = ungoal;
+		collision_flag = un_col;
+		cur_time = rt_timer_read();
 
         while (1) {
             rt_event_wait(&event,
@@ -24,6 +32,10 @@ namespace task_physics_engine {
             if (mask & event_mask::kTerminate)
                 goto PHYSICS_ENGINE_TERMINATE;
             rt_printf("[physics_engine] I'm waiting for codes...\n");
+            
+            pre_time = cur_time;
+            cur_time = rt_timer_read();
+            step_time = (cur_time - pre_time)/1000000.0;
         }
 
         PHYSICS_ENGINE_TERMINATE:
