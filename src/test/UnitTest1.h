4700e02222a2db5f943144eb74c4935ca3c6363a
U201310615 <947253826@qq.com>
2017-01-09 11:20:35 +0800

    Update UnitTest1.h

--- src/test/UnitTest1.h
+++ src/test/UnitTest1.h
@@ -5,12 +5,12 @@ CPPUNIT_TEST_SUITE_REGISTRATION(TrapezoidInterpolationTest);                    
 CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(TrapezoidInterpolationTest, "trapezoid");
 
 /**
- * @brief 测试：判断在运动过程中某一时刻的速度和位移是否正确
+ * @brief 测试：判断在正向运动过程中某一时刻的速度和位移是否正确
  */
 void TrapezoidInterpolationTest::base_function() {
     TrapezoidInterpolation algorithm;
-    algorithm.set_time(12);
-    algorithm.set_position(200);
+    algorithm.set_time(10);
+    algorithm.set_position(120);
     algorithm.set_velocity(10);
     algorithm.set_acceleration(5);
 
@@ -20,10 +20,10 @@ void TrapezoidInterpolationTest::base_function() {
     CPPUNIT_ASSERT_DOUBLES_EQUAL(10, algorithm.get_position(), 0.1);
     CPPUNIT_ASSERT_DOUBLES_EQUAL(10, algorithm.get_velocity(), 0.1);//判断加速过程中某一时刻的速度和位移是否正确
 
-    algorithm.move(8);
-    CPPUNIT_ASSERT_DOUBLES_EQUAL(130, algorithm.get_position(), 0.1);
+    algorithm.move(6);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(50, algorithm.get_position(), 0.1);
     CPPUNIT_ASSERT_DOUBLES_EQUAL(20, algorithm.get_velocity(), 0.1);//判断匀速过程中某一时刻的速度和位移是否正确
 	
-	algorithm.move(11);
-    CPPUNIT_ASSERT_DOUBLES_EQUAL(187.5, algorithm.get_position(), 0.1);
+	algorithm.move(9);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(107.5, algorithm.get_position(), 0.1);
     CPPUNIT_ASSERT_DOUBLES_EQUAL(15, algorithm.get_velocity(), 0.1);//判断减速过程中某一时刻的速度和位移是否正确
